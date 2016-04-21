614e34545eb6350995bd6c35a958a74e44fb52a1
Daniel Koskinen <dani@dani.fi>
2016-04-21 11:51:21 +0300

    Initial working cookie consent code
    Based on original code by Tomi Mäenpää and https://silktide.com/tools/cookie-consent/docs/installation/

---
+++ h1-cookie-consent.php
@@ -0,0 +1,54 @@
+<?php
+/*
+Plugin Name: Simple Cookie Consent
+Plugin URI: https://github.com/H1FI/h1-cookie-consent
+Description: A very simple cookie consent plugin designed specifically for multisite setups, although will work fine in single installs.
+Version: 0.1
+Author: Daniel Koskinen, Tomi Mäenpää, H1
+Author URI: http://h1.fi
+License: GPL2
+Textdomain: h1-cookie-consent
+*/
+/*  Copyright 2016  H1 Web Oy
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+add_action( 'plugins_loaded', function() {
+
+    load_plugin_textdomain( 'h1-cookie-consent', false, plugin_basename( dirname( __FILE__ ) ) . '/languages' );
+
+} );
+
+add_action( 'wp_enqueue_scripts', 'h1cc_scripts_styles' );
+/**
+ * Register the scripts and styles
+ */
+function h1cc_scripts_styles() {
+    //wp_register_script( 'cookieconsent2', plugins_url( 'js/cookieconsent.min.js' , __FILE__ ), null, '1.0.10', true );
+    wp_register_script( 'cookieconsent2', '//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/1.0.10/cookieconsent.min.js', null, '1.0.10', true );
+
+    $cookie_consent_options = array(
+            'message' => esc_html__( 'This website uses cookies to ensure you get the best experience on our website.', 'h1-cookie-consent' ),
+            'dismiss' => esc_html__( 'Got it!', 'h1-cookie-consent' ),
+            'learnMore' => esc_html__( 'More info', 'h1-cookie-consent' ),
+            'link' => false,
+            'theme' => plugins_url( 'css/h1-cookie-consent.css', __FILE__ ),
+    );
+
+    wp_localize_script( 'cookieconsent2', 'cookieconsent_options', $cookie_consent_options );
+
+    wp_enqueue_script( 'cookieconsent2' );
+}
+
---
+++ js/cookieconsent.js
@@ -0,0 +1,12 @@
+<html>
+<head><title>404 Not Found</title><script type="text/javascript">
+//<![CDATA[
+try{if (!window.CloudFlare) {var CloudFlare=[{verbose:0,p:0,byc:0,owlid:"cf",bag2:1,mirage2:0,oracle:0,paths:{cloudflare:"/cdn-cgi/nexp/dok3v=1613a3a185/"},atok:"3624fd8b1555b4585cf3114e5ee257ee",petok:"7800f621594e7b9e7be99338ae0c50dd45815e78-1461226899-1800",adblock:1,betok:"2d9f4470065c3a3bb28bfdf929ae5d7e0628a0d7-1461226899-120",zone:"cloudflare.com",rocket:"0",apps:{},sha2test:0}];!function(a,b){a=document.createElement("script"),b=document.getElementsByTagName("script")[0],a.async=!0,a.src="//ajax.cloudflare.com/cdn-cgi/nexp/dok3v=e982913d31/cloudflare.min.js",b.parentNode.insertBefore(a,b)}()}}catch(e){};
+//]]>
+</script>
+</head>
+<body bgcolor="white">
+<center><h1>404 Not Found</h1></center>
+<hr><center>nginx</center>
+</body>
+</html>
